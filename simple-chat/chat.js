d316b6c20a21f5d014b2314fee179cf9f3876f58
Adem's Lab <ademslab@hotmail.com>
2013-01-07 20:37:06 +0100

    simple chat v0.1

---
+++ simple-chat/chat.js
@@ -0,0 +1,40 @@
+//MODULES
+var http = require('http')
+var io = require('socket.io')
+
+//VARS
+var server_port = 4444;
+
+//HTTP SERVER
+//create http server
+var server = http.createServer()
+//set listen port for http server
+server.listen(server_port)
+
+//MESSAGES
+//this is where we store messages
+var messages = new Array()
+//define save method for Array
+Array.prototype.save = function(element) {
+    this.push(element)
+}
+
+//SOCKET.IO
+//set listen for specific http server
+io=io.listen(server)
+//set options
+io.set ('transports', ['xhr-polling', 'jsonp-polling'])
+io.set('log level', 1); //remove junk log messages
+//define events
+io.sockets.on('connection', function(client) {
+	//send all messages to newly connected client
+    client.emit("init", JSON.stringify(messages))
+    client.on('msg', function(msg) {
+		//parse json message
+        var newmessage = JSON.parse(msg)
+		//save it to message store
+        messages.save(newmessage)
+		//send new message to all connected clients
+        client.broadcast.emit('msg', msg)
+    })
+})
